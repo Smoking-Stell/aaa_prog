1dfd857f85df77f4a561400597b848eccabc014d
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2023-10-21 17:01:33 +0300

    fix tests, add doc comments, add practice solutions

--- task3/CusCountVectorizer.py
+++ task3/CusCountVectorizer.py
@@ -3,17 +3,32 @@ import string
 
 class CusCountVectorizer:
     def __init__(self, vocabulary: dict = None):
+        """Initialization of vocabulary for class
+
+        :param vocabulary: if given make the vocabulary
+        """
         if vocabulary is None:
             self.vocabulary_ = {}
         else:
             self.vocabulary_ = vocabulary
 
     @staticmethod
-    def _preprocess(text):
+    def _preprocess(text: string) -> string:
+        """Format string to lower case and delete punctuation symbols
+
+        :param text: unformatted string
+        :return: formatted string
+        """
         return text.lower().translate(
             str.maketrans('', '', string.punctuation))
 
-    def fit(self, documents):
+    def fit(self, documents: list):
+        """Find all unique words in documents and fill the vocabulary
+        in order of receiving words: word is key, and it's number is value
+
+        :param documents: list of texts where we need to find all unique words
+        :return: CusCountVectorizer
+        """
         word_id = 0
         for doc in documents:
             for word in doc.split():
@@ -23,7 +38,15 @@ class CusCountVectorizer:
                     word_id += 1
         return self
 
-    def transform(self, documents):
+    def transform(self, documents: list) -> list:
+        """Matches words with words in the dictionary and make a vector
+        of words amounts for each document.
+        Indexes of vector are from vocabulary
+
+        :param documents: list of documents where we need
+         to find amount of words
+        :return: list of vectors for documents
+        """
         vectors = []
         for doc in documents:
             vector = [0] * len(self.vocabulary_)
@@ -34,12 +57,27 @@ class CusCountVectorizer:
             vectors.append(vector)
         return vectors
 
-    def fit_transform(self, documents):
+    def fit_transform(self, documents: list) -> list:
+        """Combine fit and transform
+
+        :param documents: list of documents where we need
+         to find amount of words after making a vocabulary
+        :return: list of vectors for documents
+        """
         self.fit(documents)
         return self.transform(documents)
 
-    def get_feature_names_out(self):
+    def get_feature_names_out(self) -> list:
+        """ Make an ordered sequence of words for the vocabulary.
+        Order of receiving
+
+        :return: list of words from the vocabulary
+        """
         return sorted(self.vocabulary_, key=self.vocabulary_.get)
 
     def get_feature_names(self):
+        """Old version of get_feature_names_out
+
+        :return:
+        """
         return self.get_feature_names_out()
--- task3/VectorizerTest.py
+++ task3/VectorizerTest.py
@@ -43,8 +43,36 @@ class TestCustomCountVectorizer(unittest.TestCase):
         docs = ["здравствуйте, ДОРОГОЙ: ;,Мартин", "поехали, туда"]
         self.vectorizer.fit(docs)
         feature_names = self.vectorizer.get_feature_names()
-        expected_names = ['здравствуйте', 'дорогой', "мартин", "поехали", "туда"]
+        expected_names = ['здравствуйте', 'дорогой',
+                          "мартин", "поехали", "туда"]
         self.assertEqual(feature_names, expected_names)
+
+    def test_fit_transform3(self):
+        docs = [
+            "Crock Pot Pasta Never boil pasta again",
+            "Pasta Pomodoro Fresh ingredients Parmesan to taste"
+        ]
+        vectors = self.vectorizer.fit_transform(docs)
+
+        expected_vectors = [
+            [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+        ]
+        self.assertEqual(vectors, expected_vectors)
+
+    def test_get_feature_names3(self):
+        docs = [
+            "Crock Pot Pasta Never boil pasta again",
+            "Pasta Pomodoro Fresh ingredients Parmesan to taste"
+        ]
+        self.vectorizer.fit(docs)
+        feature_names = self.vectorizer.get_feature_names()
+
+        expected_names = ['crock', 'pot', 'pasta', 'never', 'boil', 'again',
+                          'pomodoro', 'fresh', 'ingredients',
+                          'parmesan', 'to', 'taste']
+        self.assertEqual(feature_names, expected_names)
+
 
 if __name__ == '__main__':
     unittest.main()
