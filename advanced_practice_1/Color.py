d0476413ad35062c81997c68842280059884219e
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2023-12-09 03:30:31 +0300

    rmul rebased

--- advanced_practice_1/Color.py
+++ advanced_practice_1/Color.py
@@ -9,7 +9,7 @@ class ComputerColor(ABC):
         pass
 
     def __rmul__(self, other: float):
-        pass
+        return self.__mul__(other)
 
 
 class Color(ComputerColor):
@@ -70,9 +70,6 @@ class Color(ComputerColor):
 
         return Color._init_limited(*new_colors)
 
-    def __rmul__(self, other):
-        return self.__mul__(other)
-
 
 def print_a(color: ComputerColor):
     bg_color = 0.2 * color
@@ -80,17 +77,20 @@ def print_a(color: ComputerColor):
         [bg_color] * 19,
         [bg_color] * 9 + [color] + [bg_color] * 9,
         [bg_color] * 8 + [color] * 3 + [bg_color] * 8,
-        [bg_color] * 7 + [color] * 2 + [bg_color] + [color] * 2 + [bg_color] * 7,
-        [bg_color] * 6 + [color] * 2 + [bg_color] * 3 + [color] * 2 + [bg_color] * 6,
+        [bg_color] * 7 + [color] * 2 + [bg_color] +
+                                        [color] * 2 + [bg_color] * 7,
+        [bg_color] * 6 + [color] * 2 + [bg_color] * 3 +
+                                        [color] * 2 + [bg_color] * 6,
         [bg_color] * 5 + [color] * 9 + [bg_color] * 5,
-        [bg_color] * 4 + [color] * 2 + [bg_color] * 7 + [color] * 2 + [bg_color] * 4,
-        [bg_color] * 3 + [color] * 2 + [bg_color] * 9 + [color] * 2 + [bg_color] * 3,
+        [bg_color] * 4 + [color] * 2 + [bg_color] * 7 +
+                                        [color] * 2 + [bg_color] * 4,
+        [bg_color] * 3 + [color] * 2 + [bg_color] * 9 +
+                                        [color] * 2 + [bg_color] * 3,
         [bg_color] * 19,
     ]
 
     for row in a_matrix:
         print(''.join(str(ptr) for ptr in row))
-
 
 
 def main():
